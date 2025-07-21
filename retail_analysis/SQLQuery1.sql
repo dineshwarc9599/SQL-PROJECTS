303e450686fe1ca9415336c234ae3b781d46710a
Dineshwar C <dineshwareshwar7099@gmail.com>
2025-07-21 18:15:39 +0530

    Add files via upload

---
+++ retail_analysis/SQLQuery1.sql
@@ -0,0 +1,75 @@
+-- find top 10 highest revenue generating produts
+
+with top_orders_cte as(select product_id,sum(sale_price) as total_sales from df_orders 
+group by product_id )
+
+select top 10 * from top_orders_cte order by total_sales desc ;
+
+--select top 5 highest selling project in each region
+select * from df_orders;
+
+with top_region as( select product_id,region,category,sum(sale_price) as total_sales ,
+rank() over(partition by region order by sum(sale_price) desc) as rank_in_region from df_orders group by product_id,region,category)
+
+select top 5 * from top_region;
+
+--which sub category had highest growth by profit in 2023 compare to 2022
+with cte as (
+select year(order_date) as order_year,month(order_date) as order_month,
+sum(sale_price) as sales
+from df_orders
+group by year(order_date),month(order_date)
+--order by year(order_date),month(order_date)
+	)
+select order_month
+, sum(case when order_year=2022 then sales else 0 end) as sales_2022
+, sum(case when order_year=2023 then sales else 0 end) as sales_2023
+from cte 
+group by order_month
+order by order_month
+
+
+
+
+
+--for each category which month had highest sales 
+with cte as (
+select category,format(order_date,'yyyyMM') as order_year_month
+, sum(sale_price) as sales 
+from df_orders
+group by category,format(order_date,'yyyyMM')
+--order by category,format(order_date,'yyyyMM')
+)
+select * from (
+select *,
+row_number() over(partition by category order by sales desc) as rn
+from cte
+) a
+where rn=1
+
+
+
+
+
+
+--which sub category had highest growth by profit in 2023 compare to 2022
+with cte as (
+select sub_category,year(order_date) as order_year,
+sum(sale_price) as sales
+from df_orders
+group by sub_category,year(order_date)
+--order by year(order_date),month(order_date)
+	)
+, cte2 as (
+select sub_category
+, sum(case when order_year=2022 then sales else 0 end) as sales_2022
+, sum(case when order_year=2023 then sales else 0 end) as sales_2023
+from cte 
+group by sub_category
+)
+select top 1 *
+,(sales_2023-sales_2022)
+from  cte2
+order by (sales_2023-sales_2022) desc
+
+
